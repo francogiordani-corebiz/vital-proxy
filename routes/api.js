a68e81e0f954ee38a687f2c3e4b77c10ed0861b3
Franco Giordani <franco.giordani@corebiz.ag>
2021-03-16 17:12:08 -0600

    rerouting

--- routes/api.js
+++ routes/api.js
@@ -4,23 +4,15 @@ const express = require('express')
 const api = require('../src/api')
 const router = express.Router()
 
-const apicache = require('apicache')
 const verifySeller = require('../src/middlewares/verifySeller')
-const cache = apicache.middleware
+
+// const sellerRouter = require('./sellerRouter')
 
 router.get('/ping', api.ping)
-router.get('/cache/performance', (req, res) => {
-  res.json(cache.getPerformance())
-})
-router.get(
-  '/promociones/:seller',
-  cache('30 minutes'),
-  verifySeller,
-  api.promotionsBySeller
-)
-router.get(
-  '/inventory/:seller/:sku',
-  cache('30 minutes'),
+
+router.use('/seller/:seller/promotions', verifySeller, api.promotionsBySeller)
+router.use(
+  '/seller/:seller/inventory/:sku',
   verifySeller,
   api.inventoryBySellerAndSku
 )
