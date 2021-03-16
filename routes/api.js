cf5633479d82668bb99387bbba50165e77e9b9d4
Franco Giordani <franco.giordani@corebiz.ag>
2021-03-16 16:28:48 -0600

    verify seller middleware

--- routes/api.js
+++ routes/api.js
@@ -5,13 +5,24 @@ const api = require('../src/api')
 const router = express.Router()
 
 const apicache = require('apicache')
+const verifySeller = require('../src/middlewares/verifySeller')
 const cache = apicache.middleware
 
 router.get('/ping', api.ping)
 router.get('/cache/performance', (req, res) => {
   res.json(cache.getPerformance())
 })
-router.get('/promociones/:seller', cache('30 minutes'), api.promotionsBySeller)
-router.get('/inventory/:seller/:sku', api.inventoryBySellerAndSku)
+router.get(
+  '/promociones/:seller',
+  cache('30 minutes'),
+  verifySeller,
+  api.promotionsBySeller
+)
+router.get(
+  '/inventory/:seller/:sku',
+  cache('30 minutes'),
+  verifySeller,
+  api.inventoryBySellerAndSku
+)
 
 module.exports = router
