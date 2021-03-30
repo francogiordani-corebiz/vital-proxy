5ab30b0f568fd20445e150b5e91af383e9b3713b
Franco Giordani <franco.giordani@corebiz.ag>
2021-03-29 19:18:22 -0600

    ofertas masterdata

--- src/api/promotions.js
+++ src/api/promotions.js
@@ -74,7 +74,7 @@ const promotionsBySeller = (req, res, next) => {
                   idCalculatorConfiguration: oferta.IdOferta,
                   isActive: oferta.isActive,
                   description: oferta.generalValues.CantidadMinima
-                    ? `A partir de ${oferta.CantidadMinima}`
+                    ? `A partir de ${oferta.generalValues.CantidadMinima}`
                     : '',
                   type: 'nominal',
                   status: oferta.isActive ? 'active' : '',
